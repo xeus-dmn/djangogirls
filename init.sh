10d773259010b97a51ab324b51eb4c53363d87d8
Брюханов Константин Владимирович (4036050) <kvbruhanov@vtb.ru>
2021-12-27 21:03:21 +0300

    init

---
+++ init.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+
+# User credentials
+user=admin
+email=admin@example.com
+password=pass
+
+file=db/db.sqlite3
+#if [ -z "$file" ]; then
+#  echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
+#fi
+
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
+python3 manage.py migrate
